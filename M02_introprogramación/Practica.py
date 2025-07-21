d94dfb8939a3728b66798972595c0abbb7f98b29
Flavio Bevilacqua <94021594+FlaBevilacqua@users.noreply.github.com>
2025-07-21 20:25:07 -0300

    Update Practica.py

--- M02_introprogramación/Practica.py
+++ M02_introprogramación/Practica.py
@@ -1,8 +1,6 @@
 
 # Este es un archivo de prueba, sin actividades =) .
 
-# A partir del próximo módulo: M03_variablesydatos vas a tener actividades para realizar.
-
 print('Hola Mundo!')
 a = 20
 while a > 0:
